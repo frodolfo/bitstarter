40296ef5cc7317cf814fe652193d4d1c1963952f
Fred Rodolfo <fred.rodolfo@gmail.com>
2013-07-07 22:56:50 +0000

    Added declaration for 'fs'

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   response.send(fs.readFileSync('index.html').toString());
+    var fs = require('fs');
+    response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
