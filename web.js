ba31e65d0d087d25d1947c333629bc2f8d2b7a87
Fred Rodolfo <fred.rodolfo@gmail.com>
2013-07-07 22:54:31 +0000

    reverted to using readFileSync

--- web.js
+++ web.js
@@ -1,10 +1,8 @@
 var express = require('express');
-var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = fs.readFile('index.html');
-    response.send(buf.toString());
+   response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
