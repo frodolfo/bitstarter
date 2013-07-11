ea9d0452df693e6d1f08dfe964bae29039e383ce
Fred Rodolfo <fred.rodolfo@gmail.com>
2013-07-11 06:27:21 +0000

    Added ability to specify remote URL

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLREMOTEFILE_DEFAULT = "remote-index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,6 +39,18 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var buildfn = function(indexHtmlFile) {
+    var responseToHtmlFile = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", indexHtmlFile);
+            fs.writeFileSync(indexHtmlFile, result);
+        }
+    };
+    return responseToHtmlFile;
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -44,7 +59,18 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtmlFile = function(urlToFile, htmlfile, checksfile) {
+    // --url param takes precendence over local file input
+    if (urlToFile) {
+	console.log("urlToFile: " + urlToFile);
+	var remoteHtmlFile = HTMLREMOTEFILE_DEFAULT; 
+	var responseToHtmlFile = buildfn(remoteHtmlFile);
+	rest.get(urlToFile).on('complete', responseToHtmlFile);
+	console.log("remoteHtmlFile: " + remoteHtmlFile);
+	htmlfile = remoteHtmlFile;
+    }
+
+    console.log("htmlfile: " + htmlfile);
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -65,10 +91,14 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_to_index.html>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson = checkHtmlFile(program.url, program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    // Write outJson to output.json
+    //fs.writeFileSync("output.json", outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
